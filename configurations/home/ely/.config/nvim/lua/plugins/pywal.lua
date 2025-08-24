c3300bdc559f95e4e2048556b6267c383e75c61c
Justo <justomelone@gmail.com>
2025-08-24 19:30:43 -0300

    Update: Remade Nvim config files

--- configurations/home/ely/.config/nvim/lua/plugins/pywal.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "AlphaTechnolog/pywal.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-        -- Set up pywal and load the colors
-        require("pywal").setup()
-    end,
-}

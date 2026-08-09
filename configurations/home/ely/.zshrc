33b91ed6c5d7675e4995c4f2ec9688790741d79a
Justo <justomlo@proton.me>
2026-08-09 20:22:03 -0300

    Update

--- configurations/home/ely/.zshrc
+++ configurations/home/ely/.zshrc
@@ -4,6 +4,8 @@
 if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
   source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
+source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 # If you come from bash you might have to change your $PATH.
 # export PATH=$HOME/bin:$HOME/.local/bin:/usr/local/bin:$PATH
--- install.sh
+++ install.sh
@@ -38,9 +38,33 @@ info "Arch Linux confirmado."
 WARNINGS=()
 
 PACMAN_PKGS=(
+    # Base / Utilities
     git
+    base-devel
+    zsh
+    zsh-autosuggestions
+    zsh-syntax-highlighting
+    vim
+    neovim
 
-    # Desktop
+    # Security / Network / Services
+    wpa_supplicant
+    ufw
+    cups
+    cups-pk-helper
+    system-config-printer
+
+    # System tools
+    btop
+    fastfetch
+    brightnessctl
+    power-profiles-daemon
+    udiskie
+    udisks2
+    gnome-keyring
+    gvfs
+
+    # Desktop environment
     quickshell
     hyprland
     hyprsunset
@@ -54,16 +78,6 @@ PACMAN_PKGS=(
     grim
     slurp
 
-    # System
-    btop
-    fastfetch
-    brightnessctl
-    power-profiles-daemon
-    udiskie
-    udisks2
-    gnome-keyring
-    gvfs
-
     # File manager
     nemo
     nemo-fileroller
@@ -74,7 +88,12 @@ PACMAN_PKGS=(
     noise-suppression-for-voice
     pipewire
     pipewire-pulse
+    pipewire-alsa
+    pipewire-jack
     wireplumber
+    gst-plugin-pipewire
+    gst-plugins-base-libs
+    pavucontrol
     playerctl
     mpd
 
@@ -85,25 +104,29 @@ PACMAN_PKGS=(
     bluez-utils
     blueman
 
+    # Graphics / OpenCL
+    ocl-icd
+    opencl-icd-loader
+    vulkan-tools
+    glfw
+
     # Gaming
     steam
-    vulkan-tools
     gamescope
     mangohud
     umu-launcher
     protontricks
     winetricks
     lutris
+    goverlay
+
+    # Gaming libraries
     openal
     libva
     libjpeg
     giflib
     mpg123
     alsa-plugins
-    glfw
-    gst-plugins-base-libs
-    opencl-icd-loader
-    goverlay
 
     # 32-bit gaming libraries
     lib32-mangohud
@@ -116,9 +139,6 @@ PACMAN_PKGS=(
     lib32-giflib
     lib32-gtk3
 
-    # Graphics
-    vulkan-tools
-
     # Desktop applications
     firefox
     mpv
@@ -127,13 +147,20 @@ PACMAN_PKGS=(
     kate
     obsidian
     zed
+    bitwarden
+    discord
+    qbittorrent
+    partitionmanager
 
-    # Qt / theming
+    # GTK / Qt / Theming
     xdg-desktop-portal-hyprland
     qt5ct
     qt6ct
     kvantum
     kvantum-qt5
+    adw-gtk-theme
+    nwg-look
+    papirus-icon-theme
 
     # Fonts
     ttf-jetbrains-mono-nerd
@@ -142,11 +169,13 @@ PACMAN_PKGS=(
     noto-fonts-cjk
     noto-fonts-emoji
     noto-fonts-extra
+    ttf-liberation
 )
 
 AUR_PKGS=(
   awww # (swww has been renamed)
-  vesktop-bin
+  # vesktop-bin
+  zsh-theme-powerlevel10k-git
   opencode
   vicinae-bin
   hayase-desktop-bin
@@ -237,6 +266,31 @@ install_sddm_if_needed() {
   success "SDDM installed and enabled."
 }
 
+install_sddm_theme() {
+  info "Installing SilentSDDM theme..."
+
+  local tmp
+  tmp=$(mktemp -d)
+
+  git clone https://github.com/uiriansan/SilentSDDM.git "$tmp/SilentSDDM"
+
+  if [[ -f "$tmp/SilentSDDM/install.sh" ]]; then
+    chmod +x "$tmp/SilentSDDM/install.sh"
+
+    (
+      cd "$tmp/SilentSDDM"
+      ./install.sh
+    )
+
+    success "SilentSDDM theme installed."
+  else
+    warn "SilentSDDM install.sh not found, skipping."
+    WARNINGS+=("SilentSDDM theme installation failed.")
+  fi
+
+  rm -rf "$tmp"
+}
+
 copy_dotfiles() {
   info "Copying dotfiles..."
 
@@ -249,6 +303,22 @@ copy_dotfiles() {
     success ".config copied to $HOME."
   else
     warn ".config directory not found in $DOTFILES_SOURCE, skipping."
+  fi
+
+  # Zsh configuration
+
+  if [[ -f "$DOTFILES_SOURCE/.zshrc" ]]; then
+      cp "$DOTFILES_SOURCE/.zshrc" "$HOME/.zshrc"
+      success ".zshrc copied to $HOME."
+  else
+      warn ".zshrc not found in $DOTFILES_SOURCE, skipping."
+  fi
+
+  if [[ -f "$DOTFILES_SOURCE/.p10k.zsh" ]]; then
+      cp "$DOTFILES_SOURCE/.p10k.zsh" "$HOME/.p10k.zsh"
+      success ".p10k.zsh copied to $HOME."
+  else
+      warn ".p10k.zsh not found in $DOTFILES_SOURCE, skipping."
   fi
 
   # Pictures
@@ -289,13 +359,29 @@ install_heavy_pkgs() {
   done
 }
 
+setup_zsh() {
+    info "Setting default shell to zsh..."
+
+    local zsh_path
+    zsh_path=$(which zsh)
+
+    if [[ "$SHELL" != "$zsh_path" ]]; then
+        chsh -s "$zsh_path"
+        success "Default shell changed to zsh."
+    else
+        success "Zsh is already the default shell."
+    fi
+}
+
 full_install() {
   install_required_packages
   install_system_packages
   install_sddm_if_needed
+  install_sddm_theme
   install_extra_packages
   install_heavy_pkgs
   copy_dotfiles
+  setup_zsh
 
   if command -v hyprctl >/dev/null; then
     hyprctl reload
@@ -306,12 +392,14 @@ install_dependencies() {
   install_required_packages
   install_system_packages
   install_sddm_if_needed
+  install_sddm_theme
   install_extra_packages
   install_heavy_pkgs
 }
 
 update_configuration() {
   copy_dotfiles
+  setup_zsh
   if command -v hyprctl >/dev/null; then
     hyprctl reload
   fi
